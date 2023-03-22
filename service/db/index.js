76b5639cf67f0ec4e05e6b89958b724c976ea94c
Lilibibi11 <1121458718@qq.com>
2023-03-22 15:08:45 +0800

    连接线上数据库

--- service/db/index.js
+++ service/db/index.js
@@ -4,23 +4,23 @@ const dbUtils = require('./handleDb/dbUtils.js')
 /**
  * 本地
  */
-let config = {
-    host: 'localhost',//数据库地址
-    port:'3306',
-    user: 'root',//用户名，没有可不填
-    password: '123456',//密码，没有可不填
-    database: 'sale'//数据库名称,
-}
+// let config = {
+//     host: 'localhost',//数据库地址
+//     port:'3306',
+//     user: 'root',//用户名，没有可不填
+//     password: '123456',//密码，没有可不填
+//     database: 'sale'//数据库名称,
+// }
 /**
  * 线上
  */
-// let config = {
-//     host: '101.43.225.80',//数据库地址
-//     port:'3306',
-//     user: 'sale2',//用户名，没有可不填
-//     password: '123456',//密码，没有可不填
-//     database: 'sale2'//数据库名称,
-// }
+let config = {
+    host: '101.43.225.80',//数据库地址
+    port:'3306',
+    user: 'backstage',//用户名，没有可不填
+    password: 'DCA5eXsANJCpkWfF',//密码，没有可不填
+    database: 'backstage'//数据库名称 
+}
 var pool = dbUtils.create(config);
 var dbClient = new dbUtils.dbClient(pool);
 module.exports = dbClient;
